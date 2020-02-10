20d451c8bf7cb0b87f43cc0d8e14cb4532c210b1
ohaganry <hermits.nature@gmail.com>
2020-02-10 08:59:43 -0500

    updated font color

--- posTerminal/Program.cs
+++ posTerminal/Program.cs
@@ -30,11 +30,13 @@ namespace posTerminal
                     MenuItem.WriteMenu(menu);
 
                     //prompt customer for order number and store it as an int variable
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     int item = Methods.CheckRange("Which menu item would you like?", 1, menu.Count);
                     Console.WriteLine($"{menu[item - 1].Name} - {menu[item - 1].Price}");
                     Console.WriteLine(menu[item - 1].Description);
 
                     //prompt customer for quantity
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     int quantity = Methods.CheckRange("How many would you like? (0-5)", 0, 5);
 
                     for (int i = 0; i < quantity; i++)
@@ -42,7 +44,9 @@ namespace posTerminal
                         cart.Add(menu[item - 1]);
                     }
 
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("You cart:");
+                    Console.ResetColor();
 
                     foreach (MenuItem m in cart)
                     {
@@ -50,12 +54,14 @@ namespace posTerminal
                     }
 
                     receipt.CalcSubTotal(cart);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine($"Subtotal: {receipt.Subtotal:C2}");
 
                     moreItems = Methods.ValidateInput("Would you like to add more items? (y/n)");
                 }
 
                 receipt.CalcTotal();
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine($"Grand Total: {receipt.Grandtotal:C2}");
 
                 Console.WriteLine("How would you like to pay?");
@@ -69,7 +75,7 @@ namespace posTerminal
                         bool validCash = true;
                         while (validCash)
                         {
-
+                            Console.ForegroundColor = ConsoleColor.Yellow;
                             string s = (Methods.GetUserInput("Cash Tendered:"));
 
                             if (Regex.IsMatch(s, @"^((\d{3})|\d+)\.\d{2}$") || Regex.IsMatch(s, @"^(\d{1,3})$"))
@@ -80,8 +86,9 @@ namespace posTerminal
                             }
                             else
                             {
-
+                                Console.ForegroundColor = ConsoleColor.Red;
                                 Console.WriteLine("Invalid entry. Please enter a valid cash amount please.");
+                                Console.ResetColor();
 
                             }
                             if (cash.AmountTendered >= receipt.Grandtotal)
@@ -90,23 +97,31 @@ namespace posTerminal
                             }
                             else
                             {
+                                Console.ForegroundColor = ConsoleColor.Red;
                                 Console.WriteLine("more money please");
+                                Console.ResetColor();
                                 validCash = true;
                             }
                             cash.CalcChange(cash.AmountTendered, receipt.Grandtotal);
                             payType = "Cash";
                         }
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("Thank you!");
+                        Console.ResetColor();
                         break;
 
                     case 2:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Credit.ValidateCardNumber(Methods.GetUserInput("Enter card number:"));
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Credit.ValidateExpirationDate(Methods.GetUserInput("Enter expiration date:"));
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Credit.ValidateCVV(Methods.GetUserInput("Enter the CVV number:"));
                         payType = "Credit";
                         break;
 
                     case 3:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Check.ValidateCheckNumber(Methods.GetUserInput("Enter check number:"));
                         payType = "Check";
                         break;
@@ -131,9 +146,10 @@ namespace posTerminal
 
                 }
 
-
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 open = Methods.ValidateInput("Would you like to make another order? (y/n)");
             }
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Thank you, come again!");
         }
     }
